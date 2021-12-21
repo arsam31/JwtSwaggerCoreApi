9b9cff34c99de36860838b2ce5964bc1ccafe644
arsam <arsamrahmaan31@gmail.com>
2021-12-21 16:14:22 +0500

    implemented swagger and user container by using scaffold

--- JwtSwaggerCoreApi/Migrations/20211221110937_fields data type fixed.Designer.cs
+++ JwtSwaggerCoreApi/Migrations/20211221110937_fields data type fixed.Designer.cs
@@ -3,14 +3,16 @@ using JwtSwaggerCoreApi.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JwtSwaggerCoreApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211221110937_fields data type fixed")]
+    partial class fieldsdatatypefixed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- JwtSwaggerCoreApi/Startup.cs
+++ JwtSwaggerCoreApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,11 +28,20 @@ namespace JwtSwaggerCoreApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddControllers();
+
+            // I have commented the above line and added again below with additional parameters to mark the objects serialized.
             services.AddControllers().AddJsonOptions(options => {
                 options.JsonSerializerOptions.PropertyNamingPolicy = null;
             });
+
             // Resolve Database Dependency
             services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Adding Swagger config
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = " My Web API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +57,11 @@ namespace JwtSwaggerCoreApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // swagger config
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                c=>c.SwaggerEndpoint("/swagger/v1/swagger.json","My API V1"));
 
             app.UseEndpoints(endpoints =>
             {
